d813f509a66ca40df4df3ea70b5fa521653bf76c
QProgS <uu.innk@gmail.com>
2017-05-04 23:06:26 +0700

    test

--- json/src/test/java/com/developmentontheedge/beans/json/JsonFactoryTest.java
+++ json/src/test/java/com/developmentontheedge/beans/json/JsonFactoryTest.java
@@ -1,5 +1,6 @@
 package com.developmentontheedge.beans.json;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import javax.json.JsonArrayBuilder;
@@ -13,7 +14,30 @@ public class JsonFactoryTest
     {
         JsonArrayBuilder dictionary = JsonFactory.createDictionary(
                 new String[]{"foo", "bar"}, true);
-        assertEquals("[[\"0\",\"foo\"],[\"1\",\"bar\"]]",dictionary.build().toString());
+        assertEquals("[['0','foo'],['1','bar']]", oneQuotes(dictionary.build().toString()));
+    }
+
+    @Test
+    @Ignore
+    public void testDpsValuesMetaDictionaries() throws Exception
+    {
+        JsonArrayBuilder dictionary = JsonFactory.createDictionary(
+                new String[]{"foo", "bar"}, true);
+        assertEquals("{"+
+                   "values:['p1':'a'," +
+                           "'p2':{'c1':'p21','c2':'p22'}" +
+                           "]," +
+                   "meta:['p1':{'title':'p1','readOnly':true}," +
+                         "'p2':{'title':'p2','readOnly':true}," +
+                         "'/p2/c1':{title:'c1','readOnly':true}" +
+                         "]," +
+                   "dictionaries:{'p1':{'key1':'value1','key2':'value2'}" +
+        "}", oneQuotes(dictionary.build().toString()));
+    }
+
+    private String oneQuotes(String s)
+    {
+        return s.replace("\"", "'");
     }
 
 }
