9885bf68ed30d9d3191b46e734c55621c696b222
Innokentiy <uu.innk@gmail.com>
2017-05-13 00:44:39 +0700

    add test

--- json/src/test/java/com/developmentontheedge/beans/jsontest/JsonFactoryDynamicPropertyAttributesTest.java
+++ json/src/test/java/com/developmentontheedge/beans/jsontest/JsonFactoryDynamicPropertyAttributesTest.java
@@ -25,36 +25,50 @@ public class JsonFactoryDynamicPropertyAttributesTest implements JsonPropertyAtt
     public void testCanBeNull()
     {
         dynamicProperty.setCanBeNull(true);
+        assertEquals(true, JsonFactory.getMeta(dynamicProperty).getBoolean(CAN_BE_NULL_ATTR));
 
-        assertEquals(true, JsonFactory.getMeta(dynamicProperty)
-                .getBoolean(CAN_BE_NULL_ATTR));
+        dynamicProperty.setCanBeNull(false);
+        assertEquals(false, JsonFactory.getMeta(dynamicProperty).containsKey(CAN_BE_NULL_ATTR));
     }
 
     @Test
     public void testReloadOnChange()
     {
         dynamicProperty.setAttribute(BeanInfoConstants.RELOAD_ON_CHANGE, true);
+        assertEquals(true, JsonFactory.getMeta(dynamicProperty).getBoolean(RELOAD_ON_CHANGE_ATTR));
 
-        assertEquals(true, JsonFactory.getMeta(dynamicProperty)
-                .getBoolean(RELOAD_ON_CHANGE_ATTR));
+        dynamicProperty.setAttribute(BeanInfoConstants.RELOAD_ON_CHANGE, false);
+        assertEquals(false, JsonFactory.getMeta(dynamicProperty).containsKey(RELOAD_ON_CHANGE_ATTR));
     }
 
     @Test
     public void testReadonly()
     {
         dynamicProperty.setReadOnly(true);
+        assertEquals(true, JsonFactory.getMeta(dynamicProperty).getBoolean(READONLY_ATTR));
 
-        assertEquals(true, JsonFactory.getMeta(dynamicProperty)
-                .getBoolean(READONLY_ATTR));
+        dynamicProperty.setReadOnly(false);
+        assertEquals(false, JsonFactory.getMeta(dynamicProperty).containsKey(READONLY_ATTR));
     }
 
     @Test
     public void testHidden()
     {
         dynamicProperty.setHidden(true);
+        assertEquals(true, JsonFactory.getMeta(dynamicProperty).getBoolean(HIDDEN_ATTR));
 
-        assertEquals(true, JsonFactory.getMeta(dynamicProperty)
-                .getBoolean(HIDDEN_ATTR));
+        dynamicProperty.setHidden(false);
+        assertEquals(false, JsonFactory.getMeta(dynamicProperty).containsKey(HIDDEN_ATTR));
+    }
+
+    @Test
+    public void testRawValue()
+    {
+        dynamicProperty.setAttribute(BeanInfoConstants.RAW_VALUE, true);
+        assertEquals(true, JsonFactory.getMeta(dynamicProperty).getBoolean(RAW_VALUE_ATTR));
+
+        dynamicProperty.setAttribute(BeanInfoConstants.RAW_VALUE, false);
+        assertEquals(false, JsonFactory.getMeta(dynamicProperty).containsKey(RAW_VALUE_ATTR));
     }
 
 }
