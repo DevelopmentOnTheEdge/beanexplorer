210b637a6058c099b0e69bbb882600662e550c3b
uuinnk <uuinnk@developmentontheedge.com>
2017-05-16 18:44:28 +0700

    init DynamicProperty.js

--- web/bootstrap/react/dps-field-box/src/App.js
+++ web/bootstrap/react/dps-field-box/src/App.js
@@ -1,21 +1,120 @@
 import React, { Component } from 'react';
+import DynamicProperty from './DynamicProperty';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.state = {fields: [
+      {
+        "name":"previousValue",
+        "title":"Предыдущие показания",
+        "isReadOnly":true,
+        "canBeNull":false,
+        "reloadOnChange":false,
+        "autoRefresh":false,
+        "type":"textInput",
+        "value":"52.000",
+        "options":[
+        ],
+        "tips":{
+        }
+      },
+      {
+        "name":"currentConsumption",
+        "title":"Текущий расход",
+        "isReadOnly":false,
+        "canBeNull":false,
+        "reloadOnChange":true,
+        "autoRefresh":false,
+        "type":"textInput",
+        "value":"0.000",
+        "options":[
+        ],
+        "tips":{
+        }
+      }
+    ]};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  _onFieldChange(name, value) {
+    const field = this.state.fields.find(field => field.name === name);
+    field.value = value;
+
+    // implicit state change => forceUpdate
+//    this.forceUpdate(() => {
+//      this.setState({ allFieldsFilled: this._allFieldsFilled() });
+//
+//      if (field.reloadOnChange || field.autoRefresh) {
+//        this._reloadOnChange(name);
+//      }
+//    });
+  }
+
+  _createFields() {
+    let curGroup = [];
+    let curGroupName = null, curGroupId = null;
+    let result = [];
+
+    const finishGroup = () => {
+      if(curGroup.length > 0) {
+        if(curGroupId) {
+          result.push(this._createGroup(curGroup, curGroupId, curGroupName));
+        } else {
+          Array.prototype.push.apply(result, curGroup);
+        }
+      }
+      curGroup = [];
+    };
+
+    for(const json of this.state.fields) {
+      const newGroup = json.group;
+      const newGroupName = newGroup ? newGroup.name : null;
+      const newGroupId = newGroup ? newGroup.id : null;
+      if(newGroupId !== curGroupId) {
+        finishGroup();
+        curGroupName = newGroupName;
+        curGroupId = newGroupId;
+      }
+      const field = (<DynamicProperty value={json} ref={json.name} key={json.name} onChange={this._onFieldChange}/>);
+      curGroup.push(field);
+    }
+    finishGroup();
+    return result;
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
       </div>
+      <div className="App-intro">
+        <form onSubmit={this.handleSubmit}>
+          {this._createFields()}
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    </div>
     );
   }
+
 }
 
 export default App;
---
+++ web/bootstrap/react/dps-field-box/src/DynamicProperty.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+
+class DynamicProperty extends Component {
+
+
+  render() {
+    const json = this.props.value;
+    const value = this.props.value.value;
+    const id = json.name + "Field";
+    const changeHandler = this.changeHandler;
+    const _this = this;
+
+    const controls = {
+      checkBox: {
+        normal: function() {
+          return ( React.DOM.input({id: id, key: id, type: "checkbox", checked: value, onChange: changeHandler}));
+        },
+        readOnly: function() {
+          return ( React.DOM.input({id: id, key: id, type: "checkbox", disabled: true, checked: value}) );
+        }
+      },
+      comboBox: {
+        normal: () => {
+          var options = json.options.map(function(option) {
+            return ( React.DOM.option({key: option.value, value: option.value}, option.text) );
+          });
+          if(json.canBeNull){
+            options.unshift(  ( React.DOM.option({key: "", value: ""}, "all") ) );
+          }
+          return (
+            React.DOM.select({id: id, ref: 'editableComboBox', key: id, defaultValue: value, onChange: changeHandler, className: this.props.controlClassName},
+              options
+            )
+          );
+        },
+        readOnly: () => {
+          const selectedOption = json.options.filter(option => option.value === json.value);
+          const text = selectedOption.length ? selectedOption[0].text : json.value;
+          return this.createStatic(text);
+        }
+      },
+      textArea: {
+        normal: () => (
+          <textarea placeholder={json.tips.placeholder} id={id} key={id} rows={json.rows} cols={json.columns} value={value} onChange={changeHandler} className={this.props.controlClassName}/>
+        ),
+        readOnly: () => this.createStatic(value)
+      },
+      textInput: {
+        normal: <input type="text" placeholder={json.tips.placeholder} id={id} key={id} value={value} />,
+        readOnly: this.createStatic(value)
+      },
+      passwordInput: {
+        normal: <input type="password" placeholder={json.tips.placeholder} id={id} key={id} value={value}
+                       onChange={changeHandler} className={this.props.controlClassName}/>,
+        readOnly: this.createStatic('******')
+      }
+    };
+
+    const renderer = controls[json.type] || controls['textInput'];
+    const valueControl = renderer[json.isReadOnly ? 'readOnly' : 'normal'];
+    const label = <label htmlFor={id} className={this.props.labelClassName}>{json.title}</label>;
+    const helpTextElement = json.tips.helpText ? <small className="text-muted">{json.tips.helpText}</small> : undefined;
+    const hasDanger = !json.canBeNull && value === '' ? 'has-error' : '';
+
+    return (
+      <fieldset className={this.props.className + ' ' + hasDanger}>
+        {label}
+        {valueControl}
+        {helpTextElement}
+      </fieldset>
+    );
+  }
+
+  createStatic(value) {
+    return <p className="form-control-static" dangerouslySetInnerHTML={{__html: value}}></p>;
+  }
+
+}
+
+export default DynamicProperty;
