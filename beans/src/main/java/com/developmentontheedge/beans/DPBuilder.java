0ccac387589c777646205a9491c8c500aae4d910
uuinnk <uuinnk@developmentontheedge.com>
2019-02-21 17:16:01 +0700

    DPBuilder - add params

--- beans/src/main/java/com/developmentontheedge/beans/DPBuilder.java
+++ beans/src/main/java/com/developmentontheedge/beans/DPBuilder.java
@@ -4,19 +4,31 @@ public class DPBuilder
 {
     private final static String UNSETTLED_CONSTANT_STRING_VALUE = "UNSETTLED_CONSTANT_STRING_VALUE";
     private final static String[][] UNSETTLED_CONSTANT_TAGS_VALUE =
-            new String[][]{new String[]{"UNSETTLED_CONSTANT_STRING_VALUE"}};
+            new String[][]{new String[]{UNSETTLED_CONSTANT_STRING_VALUE}};
     private final DynamicProperty property;
 
     public Object value = UNSETTLED_CONSTANT_STRING_VALUE;
     public Class<?> type = null;
     public String title = null;
+    public String[][] tags = UNSETTLED_CONSTANT_TAGS_VALUE;
 
     public Boolean hidden = null;
     public Boolean nullable = null;
     public Boolean readonly = null;
     public Boolean reloadOnChange = null;
-    public String[][] tags = UNSETTLED_CONSTANT_TAGS_VALUE;
+    public Boolean rawValue = null;
     public Boolean multiple = null;
+
+    public String groupID = null;
+    public String groupName = null;
+    public String groupClasses = null;
+
+    public String cssClasses = null;
+    public String status = null;
+    public String message = null;
+
+    public int size = -1;
+    public int columnSize = -1;
 
     public DPBuilder(String name)
     {
@@ -43,14 +55,27 @@ public class DPBuilder
     {
         if (value != UNSETTLED_CONSTANT_STRING_VALUE) property.setValue(value);
         if (type != null) property.setType(type);
+        if (title != null) property.setDisplayName(title);
+        if (tags != UNSETTLED_CONSTANT_TAGS_VALUE) attr(BeanInfoConstants.TAG_LIST_ATTR, tags);
+
         if (hidden != null) property.setHidden(hidden);
         if (nullable != null) property.setCanBeNull(nullable);
         if (readonly != null) property.setReadOnly(readonly);
         if (reloadOnChange != null) attr(BeanInfoConstants.RELOAD_ON_CHANGE, reloadOnChange);
-
-        if (title != null) property.setDisplayName(title);
-        if (tags != UNSETTLED_CONSTANT_TAGS_VALUE) attr(BeanInfoConstants.TAG_LIST_ATTR, tags);
+        if (rawValue != null) attr(BeanInfoConstants.RAW_VALUE, rawValue);
         if (multiple != null) attr(BeanInfoConstants.MULTIPLE_SELECTION_LIST, multiple);
+
+        if (groupID != null) attr(BeanInfoConstants.GROUP_ID, groupID);
+        if (groupName != null) attr(BeanInfoConstants.GROUP_NAME, groupName);
+        if (groupClasses != null) attr(BeanInfoConstants.GROUP_CLASSES, groupClasses);
+
+        if (cssClasses != null) attr(BeanInfoConstants.CSS_CLASSES, cssClasses);
+        if (status != null) attr(BeanInfoConstants.STATUS, status);
+        if (message != null) attr(BeanInfoConstants.MESSAGE, message);
+
+        if (size != -1) attr(BeanInfoConstants.INPUT_SIZE_ATTR, size);
+        if (columnSize != -1) attr(BeanInfoConstants.COLUMN_SIZE_ATTR, columnSize);
+
         return property;
     }
 }
