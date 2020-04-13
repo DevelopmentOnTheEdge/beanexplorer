0da4f86c380781596b5161a5156292b99216e388
Sergey Zhatchenko <zha@DevelopmentOnTheEdge.com>
2020-04-13 19:07:14 +0700

    missing import

--- beans/src/main/java/com/developmentontheedge/beans/DynamicPropertySetSupport.java
+++ beans/src/main/java/com/developmentontheedge/beans/DynamicPropertySetSupport.java
@@ -14,6 +14,7 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
 import java.util.Spliterator;
